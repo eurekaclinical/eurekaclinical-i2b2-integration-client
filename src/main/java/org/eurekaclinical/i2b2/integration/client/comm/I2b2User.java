9a33d6d2a8e45d54a5adfa3b3feaf0fc4034b2a3
Andrew Post <arpost@emory.edu>
2016-08-04 18:47:40 -0400

    Skeletal i2b2 user comm object.

---
+++ src/main/java/org/eurekaclinical/i2b2/integration/client/comm/I2b2User.java
@@ -0,0 +1,18 @@
+package org.eurekaclinical.i2b2.integration.client.comm;
+
+/**
+ *
+ * @author Andrew Post
+ */
+public class I2b2User {
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
